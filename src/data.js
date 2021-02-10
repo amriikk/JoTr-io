0a0cf01cec03b4db5e6fd901f2b329c9bf00cc41
John Trujillo <amriikk@gmail.com>
2021-02-09 23:04:05 -0800

    update data.js

---
+++ src/data.js
@@ -0,0 +1,55 @@
+// Project Data
+
+const projects = [
+    {
+        id: 1,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+    {
+        id: 1,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+    {
+        id: 2,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+    {
+        id: 3,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+    {
+        id: 4,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+    {
+        id: 5,
+        title: '',
+        category: '',
+        tech: '',
+        img: 'url'
+        desc: 'description'
+    },
+]
+    
+
+export default projects;
