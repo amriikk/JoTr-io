607ba1485caebe4fbebf7c1f42e34e019265c9a7
John Trujillo <amriikk@gmail.com>
2021-02-16 22:52:07 -0800

    implement category filter function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [portfolioItems, setPortfolioItems] = useState(projects);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setPortfolioItems(newItems);
+  };
+
   return (
     <main>
       <section className='portfolio'>
@@ -15,7 +20,7 @@ function App() {
           <h2 className='subtitle'>A Visionary Portfolio</h2>
           <div className='underline'></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems}/>
         <Portfolio items={portfolioItems} />
       </section>
     </main>
