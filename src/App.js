c5822d5b9bec777ca2bb535a9a449837a8e0b1f6
John Trujillo <amriikk@gmail.com>
2021-02-16 19:59:15 -0800

    touch Categories + Portfolio jsx files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React, { useState } from 'react';
+import Portfolio from './components/Portfolio';
+import Categories from './components/Categories';
+import projects from './data';
 
 function App() {
+  const [portfolioItems, setPortfolioItems] = useState(projects);
+  const [categories, setCategories] = useState([]);
+
   return (
-    <div className="App">
-      My Portflio App
-    </div>
+    <main>
+      <section className='portfolio'>
+        <div>
+          <h1>Jo.Tr.io</h1>
+          <h2>A Visionary Portfolio</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Portfolio />
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return <h2>Portfolio Component</h2>
+};
+
+export default Portfolio;
