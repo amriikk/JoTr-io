08760765c16bd18ba5006cc180a56209ebb0c108
John Trujillo <amriikk@gmail.com>
2021-02-16 20:17:15 -0800

    update portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,8 +5,13 @@ const Portfolio = ({ items }) => {
     return (
         <div className='section-center'>
             { items.map((portfolioItem) => {
-                const { id, title, category, tech, img, desc} = portfolioItem
-
+                const { id, title, category, tech, img, desc} = portfolioItem;
+                
+                return (
+                    <article key={id} className='portfolio-item'>
+                        <img src={img} alt={title} className='photo' />
+                    </article>
+                );
             })}
         </div>
     );
