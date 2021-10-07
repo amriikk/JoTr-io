2ab1f608d2f065f7c71100469cac32063e96d1c0
John Trujillo <amriikk@gmail.com>
2021-10-06 23:50:00 -0700

    add gh-links to projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ const Portfolio = ({ items }) => {
     return (
         <div className='section-center'>
             { items.map((portfolioItem) => {
-                const { id, title, category, tech, img, desc} = portfolioItem;
+                const { id, title, category, tech, img, desc, ghLink} = portfolioItem;
                 
                 return (
                     <article key={id} className='portfolio-item'>
@@ -15,6 +15,7 @@ const Portfolio = ({ items }) => {
                                 <h4>{ title }</h4>
                                 <h5 className='tech'>{ tech }</h5>
                             </header>
+                            <h6 className='gh-link'>{ ghLink }</h6>
                             <p className='item-text'>{ desc }</p>
                         </div>
                     </article>
