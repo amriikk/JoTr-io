e7b49063b01008cb7d8a6d13b39d59b3bec39c3a
John Trujillo <amriikk@gmail.com>
2021-11-09 00:31:29 -0800

    update fa icon links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaInstagram, FaFilePdf } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaInstagram, FaFilePdf } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div align="center" className='footer'>
             <br />
-            <h3><FaGithub /> <FaLinkedin /> <FaInstagram /> <FaFilePdf /></h3>
+            <h3 className="fa-links">
+                <a href="https://github.com/amriikk">
+                    <FaGithub />      
+                </a>
+                <a href="https://www.linkedin.com/in/jhonkhrizhtian/">
+                    <FaLinkedin />         
+                </a>
+                <a href="https://instagram.com/vajra.io">
+                    <FaInstagram />         
+                </a>
+                <a href='https://drive.google.com/file/d/1fmlfrL7hsSAkFjbSsqAGp2It4uamFdZ2/view?usp=sharing' download>
+                    <FaFilePdf />
+                </a>
+            </h3>
         </div>
     );
 }
